08c17c74af721f78bc32ae31e0d064a89676ccbd
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-05-19 19:05:08 -0300

    web/feat: implement list memories.

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -26,6 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
@@ -48,7 +49,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Rigth */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
